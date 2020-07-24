4d7a3ff761aa1d97bf356b7721dfd891fed78155
Matthias Busenhart <maede97@hotmail.com>
2020-07-24 15:44:20 +0200

    Add doxygen

--- RayTracerLib/include/RayTracerLib/Utils.h
+++ RayTracerLib/include/RayTracerLib/Utils.h
@@ -8,6 +8,14 @@
 
 namespace RayTracer {
 
+	/**
+	 * @brief Traces a ray with given shapes and depth
+	 * @param ray The ray to trace
+	 * @param shapes The vector of shapes
+	 * @param depth The current depth of the ray
+	 * @param MAX_RAY_DEPTH The maximum depth of the ray
+	 * @param return The color at that given point
+	*/
 	Color trace(const Ray& ray, const std::vector<Shape*>& shapes, const int& depth, const int MAX_RAY_DEPTH);
 
 	void render(const std::vector<Shape*>& shapes, unsigned width = 1920, unsigned heigth = 1080, const int MAX_RAY_DEPTH = 5, const char* filename = "untitled.ppm");
